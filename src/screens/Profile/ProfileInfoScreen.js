a2d053dc45e41333ab875ca096f10444d214bd0f
Christian Martinez <mar13137@byui.edu>
2021-05-18 22:44:57 -0500

    Added Personal Info template

--- src/screens/Profile/ProfileInfoScreen.js
+++ src/screens/Profile/ProfileInfoScreen.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, SafeAreaView, TextInput, Button } from 'react-native'
 
 export default class ProfileInfoScreen extends Component {
     render() {
         return (
-            <View>
-                <Text> ProfileInfoScreen </Text>
-            </View>
+            <SafeAreaView>
+                <View style={styles.cell}>
+                    <Text style={styles.text}>First Name</Text>
+                    <TextInput style={styles.text} placeholder='Donald' />
+                </View>
+                <View style={styles.cell}>
+                    <Text style={styles.text}>Last Name</Text>
+                    <TextInput style={styles.text} placeholder='Trump' />
+                </View>
+                <View style={styles.cell}>
+                    <Text style={styles.text}>E-mail</Text>
+                    <TextInput style={styles.text} placeholder='Donald@Trump.com' />
+                </View>
+                <View style={styles.cell}>
+                    <Text style={styles.text}>Phone Number</Text>
+                    <TextInput style={styles.text} placeholder='555-555-5555' />
+                </View>
+                <View style={styles.cell}>
+                    <Text style={styles.text}>Change Password</Text>
+                    <TextInput style={styles.text} placeholder='*******' />
+                </View>
+                <Button title='Save'/>
+            </SafeAreaView>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    cell: {
+        flexDirection: 'row',
+    },
+    text: {
+        fontSize: 25,
+        paddingRight: 10,
+    }
+})
